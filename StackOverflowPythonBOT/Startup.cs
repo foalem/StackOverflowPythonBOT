ebffc26a558ccc8d2a9af8c13120362d66f8f22f
foalem patrick loic <fpatrickloic@yahoo.fr>
2020-07-23 11:46:53 +0200

    Connexion à QnA Maker

--- StackOverflowPythonBOT/Startup.cs
+++ StackOverflowPythonBOT/Startup.cs
@@ -6,34 +6,66 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.QnA;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
 using StackOverflowPythonBOT.Bots;
+
+//using StackOverflowPythonBOT.Bots;
+//using StackOverflowPythonBOT.Dialogs;
 
 namespace StackOverflowPythonBOT
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
 
-        public IConfiguration Configuration { get; }
+        //public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ChatBOTContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("ChatBOTDatabaseConnection")));
+
+            // Add the HttpClientFactory to be used for the QnAMaker calls.
+            services.AddHttpClient();
+
             services.AddControllers().AddNewtonsoftJson();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state.
+            services.AddSingleton<UserState>();
 
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
+            // Create the Conversation state. (Used by the Dialog system itself.)
+           // services.AddSingleton<ConversationState>();
+
+            // The Dialog that will be run by the bot.
+            //services.AddSingleton<MainDialog>();
+            //services.AddSingleton<MyQnAMaker>();
+            //services.AddSingleton(new QnAMakerEndpoint
+            //{
+            //    KnowledgeBaseId = Configuration.GetValue<string>($"QnAKnowledgebaseId"),
+            //    EndpointKey = Configuration.GetValue<string>($"QnAAuthKey"),
+            //    Host = Configuration.GetValue<string>($"QnAEndpointHostName")
+            //});
+
+            // The Dialog that will be run by the bot.
+            //services.AddSingleton<DialogBot>();
+
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, EchoBot>();
+            services.AddTransient<IBot, DialogAndWelcomeBot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
